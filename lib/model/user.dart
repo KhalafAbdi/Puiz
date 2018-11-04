8008f2b44b902737d4ba157fedc1c51b7f2d04dd
Khalaf Abdifatah <khalaf.abdifatah@hotmail.com>
2018-11-04 23:53:00 +0100

    update user model to hold more info

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -4,13 +4,26 @@ class User{
   String _id = "#";
   String _displayName;
   String _email;
+  
+  int _level = 0;
+  int _points = 0;
+  int _coins = 0;
+  int _gold = 0;
+  int _completedRewards = 0;
 
   User(this._displayName, this._email);
+
+  User.fromDatabase(this._id,this._displayName,this._email,this._level, this._points,this._coins,this._gold,this._completedRewards);
 
   User.map(dynamic obj){
     this._id = obj['id'];
     this._displayName = obj['displayName'];
     this._email = obj['email'];
+    this._level = obj['level'];
+    this._points = obj['points'];
+    this._coins = obj['coins'];
+    this._gold = obj['gold'];
+    this._completedRewards = obj['completedRewards'];
   }
 
   String get displayName => _displayName;
@@ -23,6 +36,11 @@ class User{
     var map = new Map<String, dynamic>();
     map['displayName'] = _displayName;
     map['email'] = _email;
+    map['level'] = _level;
+    map['points'] = _points;
+    map['coins'] = _coins;
+    map['gold'] = _gold;
+    map['completedRewards'] = _completedRewards;
 
     return map;
   }
